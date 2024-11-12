105fccb168bbed10c14e3ba69c3db55d4f5f0636
Muhtadi1Laskar <laskar90muhtadi@gmail.com>
2024-11-12 20:08:52 +0600

    Formatted the code

--- Create Blockchain/main.py
+++ Create Blockchain/main.py
@@ -3,17 +3,18 @@ import hashlib
 import json
 from flask import Flask, jsonify
 
+
 class Blockchain:
     def __init__(self):
         self.chain = []
-        self.create_block(proof=1, previous_hash='0')
-    
+        self.create_block(proof=1, previous_hash="0")
+
     def create_block(self, proof, previous_hash):
         block = {
             "index": len(self.chain) + 1,
             "timestamp": str(datetime.datetime.now()),
             "proof": proof,
-            "previous_hash": previous_hash
+            "previous_hash": previous_hash,
         }
         self.chain.append(block)
 
@@ -21,18 +22,19 @@ class Blockchain:
 
     def get_previous_block(self):
         return self.chain[-1]
-    
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
 
         while not check_proof:
-            hash_operation = hashlib.sha256(str( new_proof ** 2 - previous_proof ** 2).encode()).hexdigest()
+            hash_operation = hashlib.sha256(
+                str(new_proof**2 - previous_proof**2).encode()
+            ).hexdigest()
 
-            if hash_operation[:4] == '0000':
+            if hash_operation[:4] == "0000":
                 check_proof = True
             else:
                 new_proof += 1
-            
-            return new_proof
-            
+
+        return new_proof
